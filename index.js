0b401b28a52acf9835d0a409d93eac60576037b9
Shubhra Biswal <shubhrabiswal08@gmail.com>
2021-08-02 23:21:49 +0530

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const cors = require('cors')
 
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,14 @@ app.use(express.json());
 //// to connect the db from db.js
 const connectDB = require('./config/db');
 connectDB();
+
+//cors 
+const corsoptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    //[`http://localhost:5000`,`http://localhost:3000`]
+}
+
+app.use(cors(corsoptions))
 
 //Template engine
 app.set('views',path.join(__dirname,'/views'))
